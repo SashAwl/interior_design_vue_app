dbcd79fcc51d19b16ac7066b0f4dd896d5efd323
SashAwl <aleks.shilova@gmail.com>
2023-06-29 21:35:01 +0300

    created page Articles and News

---
+++ script.js
@@ -0,0 +1,13 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        articles: [
+            { id: 1, type: 'Kitchen Design', src: 'image/articles/articles1.jpg', headText: 'Let’s Get Solution For Building Construction Work', data: '26 December,2022 ' },
+            { id: 2, type: 'Living Design', src: 'image/articles/articles2.jpg', headText: 'Low Cost Latest Invented Interior Designing Ideas.', data: '26 December,2022 ' },
+            { id: 3, type: 'Interior Design', src: 'image/articles/articles3.jpg', headText: 'Best For Any Office & Business Interior Solution', data: '26 December,2022 ' },
+            { id: 4, type: 'Kitchen Design', src: 'image/articles/articles4.jpg', headText: 'Let’s Get Solution For Building Construction Work', data: '26 December, 2022 ' },
+            { id: 5, type: 'Living Design', src: 'image/articles/articles5.jpg', headText: 'Low Cost Latest Invented Interior Designing Ideas.', data: '26 December, 2022 ' },
+            { id: 6, type: 'Interior Design', src: 'image/articles/articles6.jpg', headText: 'Best For Any Office & Business Interior Solution', data: '26 December, 2022 ' }
+        ]
+    }
+})
